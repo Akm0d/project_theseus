88c06b7c19dfd07402f058231966b4476f76bf75
Tyler Johnson <tjohnson@control4.com>
2018-01-25 13:54:51 -0700

    restructured for easy templates

--- python/WebApp/base/__init__.py
+++ python/WebApp/base/__init__.py
@@ -1,11 +1,11 @@
 from flask import current_app as app, render_template, request, redirect, jsonify, url_for, Blueprint, \
     abort, render_template_string, send_file
 
-base = Blueprint('base', __name__, template_folder='WebApp/frontend/templates')
+base = Blueprint('base', __name__)
 
 
 @base.route('/', methods=['GET'])
-def admin_view():
+def base_view():
     return render_template("index.html")
 
 # TODO 404 not found and other error pages
