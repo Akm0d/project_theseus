6adfd440199da5af314a2a2613d06f4043ecf926
tyler <tjohnson@control4.com>
2018-02-18 18:31:39 -0700

    tripwires have full html functions

--- python/WebApp/api/__init__.py
+++ python/WebApp/api/__init__.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, current_app as app, request
 from flask_restful import Api, Resource
+from random import choice
 
 import logging
 
@@ -10,19 +11,78 @@ blueprint = Blueprint('api', __name__, url_prefix='/api')
 api = Api(blueprint)
 
 
+@api.resource('/solenoid/<action>')
+class Solenoid(Resource):
+    def __init__(self):
+        self.enabled = True
+
+    def get(self, action: str):
+        if action == "toggle":
+            logger.debug("Toggling the solenoid")
+            # TODO make a request to the listener.  Ask to change the enabled state of the solenoid
+            pass
+
+        # TODO get the state of the solenoid from the listener instead of a random choice
+        return {"status": choice(["Open", "Closed"])}
+
+
+@api.resource('/timer/<action>')
+class Timer(Resource):
+    def __init__(self):
+        self.enabled = True
+
+    def get(self, action: str):
+        if action == "toggle":
+            logger.debug("Toggling the timer")
+            # TODO make a request to the listener.  Ask to change the enabled state of the timer
+            pass
+
+        # TODO get the state of the timer from the listener instead of a random choice
+        return {"status": choice(["Reset", "Start"])}
+
+
+@api.resource('/tripwire/<name>/<action>')
+class Tripwire(Resource):
+    def get(self, name: str, action: str):
+        toggle = action == "toggle"
+        logger.debug("Tripwire {}".format(name))
+        if toggle:
+            # TODO toggle this specific tripwire
+            pass
+
+        # TODO get the status of the tripwire.  Return green if enabled, else white
+        return {"color": choice(["red", ""])}
+
+
+@api.resource('/tripwire/all/<action>')
+class TripwireAll(Resource):
+    def get(self, action):
+        toggle = action == "toggle"
+        # TODO if at least one tripwire is on, turn them all off.  If all of them are off, then randomize or turn all on
+        status = dict()
+        for i in range(1, 7):
+            # TODO get the status of each tripwire.  Is it on or off?
+            status[i] = choice(["red", ""])
+        return status
+
+
+@api.resource('/tripwire/randomize')
+class Randomize(Resource):
+    def get(self):
+        # TODO send a request to randomize the order of the lasers then return the status of all of them
+        return TripwireAll().get(action="status")
+
+
 @api.resource('/ultrasonic/<action>')
 class Ultrasonic(Resource):
     def __init__(self):
         self.enabled = True
 
-    def get(self, action=None):
+    def get(self, action: str):
         if action == "toggle":
             logger.debug("Toggling the ultrasonic sensor")
             # TODO make a request to the listener.  Ask to change the enabled state of ultrasonic
             pass
 
-        # TODO get the state of the ultrasonic sensor
-        from random import choice
-        enabled = choice([True, False])
-
-        return {"status": "Enabled" if enabled else "Disabled"}
+        # TODO get the state of the ultrasonic sensor instead of a random choice
+        return {"status": choice(["Enabled", "Disabled"])}
--- python/WebApp/static/javascript/admin.js
+++ python/WebApp/static/javascript/admin.js
@@ -1,12 +1,112 @@
 
 // When the page is loaded, query the state of all the sensors
 $(document).ready(function(){
-    ultrasonic_status(toggle=false)
+    solenoid_status(toggle=false);
+    timer_status(toggle=false);
+    tripwire_all(toggle=false);
+    ultrasonic_status(toggle=false);
 });
+
+Array.prototype.forEach.call(document.getElementsByClassName("tripwire"), function(tripwire) {
+    tripwire.onclick = function() {
+        tripwire_status(number = tripwire.name, toggle = true)
+    }
+});
+
+document.getElementById("tripwire-all").onclick = function() {
+    tripwire_all(toggle=true)
+};
+
+document.getElementById("tripwire-randomize").onclick = function() {
+    tripwire_randomize()
+};
+
+document.getElementById("solenoid-toggle").onclick = function() {
+    solenoid_status(toggle=true)
+};
+
+document.getElementById("timer-start-reset").onclick = function() {
+    timer_status(toggle=true)
+};
 
 document.getElementById("ultrasonic-enable").onclick = function() {
     ultrasonic_status(toggle=true)
 };
+
+
+function solenoid_status(toggle){
+    var x = new XMLHttpRequest();
+    x.open('GET', 'http://' + document.location.host + '/api/solenoid' + (toggle? "/toggle": "/status"), true);
+    x.onload = function () {
+        if (x.readyState === 4) {
+            if (x.status === 200) {
+                var data = JSON.parse(x.response);
+                document.getElementById("solenoid-toggle").innerHTML = data["status"];
+            }
+        }
+    };
+    x.send();
+}
+
+function timer_status(toggle){
+    var x = new XMLHttpRequest();
+    x.open('GET', 'http://' + document.location.host + '/api/timer' + (toggle? "/toggle": "/status"), true);
+    x.onload = function () {
+        if (x.readyState === 4) {
+            if (x.status === 200) {
+                var data = JSON.parse(x.response);
+                document.getElementById("timer-start-reset").innerHTML = data["status"];
+            }
+        }
+    };
+    x.send();
+}
+
+function tripwire_status(number, toggle) {
+    var x = new XMLHttpRequest();
+    x.open('GET', 'http://' + document.location.host + '/api/tripwire/' + number + (toggle? "/toggle": "/status"), true);
+    x.onload = function () {
+        if (x.readyState === 4) {
+            if (x.status === 200) {
+                var data = JSON.parse(x.response);
+                document.getElementById("tripwire-" + number.toString()).style.backgroundColor=data["color"];
+            }
+        }
+    };
+    x.send();
+}
+
+function tripwire_all(toggle) {
+    var x = new XMLHttpRequest();
+    x.open('GET', 'http://' + document.location.host + '/api/tripwire/all' + (toggle? "/toggle": "/status"), true);
+    x.onload = function () {
+        if (x.readyState === 4) {
+            if (x.status === 200) {
+                var data = JSON.parse(x.response);
+                Array.prototype.forEach.call(document.getElementsByClassName("tripwire"), function(tripwire) {
+                    document.getElementById("tripwire-" + tripwire.name.toString()).style.backgroundColor=data[tripwire.name];
+                });
+            }
+        }
+    };
+    x.send();
+}
+
+function tripwire_randomize(toggle) {
+    var x = new XMLHttpRequest();
+    x.open('GET', 'http://' + document.location.host + '/api/tripwire/randomize' + (toggle? "/toggle": "/status"), true);
+    x.onload = function () {
+        if (x.readyState === 4) {
+            if (x.status === 200) {
+                var data = JSON.parse(x.response);
+                Array.prototype.forEach.call(document.getElementsByClassName("tripwire"), function(tripwire) {
+                    document.getElementById("tripwire-" + tripwire.name.toString()).style.backgroundColor=data[tripwire.name];
+                });
+            }
+        }
+    };
+    x.send();
+}
 
 function ultrasonic_status(toggle){
     var x = new XMLHttpRequest();
@@ -21,3 +121,4 @@ function ultrasonic_status(toggle){
     };
     x.send();
 }
+
