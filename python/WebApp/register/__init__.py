0290fe350653ad07137001086cec7a94a6c0fff2
Tyler Johnson <jtylers@byu.edu>
2018-02-21 21:49:59 -0700

    removed register class

--- python/WebApp/register/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import current_app as app, render_template, request, redirect, jsonify, url_for, Blueprint, \
-    abort, render_template_string, send_file
-
-register = Blueprint('register', __name__)
-
-
-@register.route('/register', methods=['GET'])
-def admin_view():
-    return "TODO Register"
