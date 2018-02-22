0ac782dc87abd7095d8a01518d617937f2bf1ccd
Charles Tolley <charlestolley@gmail.com>
2018-02-21 21:07:25 -0700

    Fork game logic

--- python/serve.py
+++ python/serve.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-from flask_apscheduler import APScheduler
-from game.logic import Logic
-from WebApp import app
+import os
 
-app.run(debug=True, threaded=True, host="127.0.0.1", port=5000)
-
-app.scheduler = APScheduler(app=app, scheduler=Logic())
-app.scheduler.start()
+if __name__ == '__main__':
+    if os.fork() == 0:
+        from game.logic import Logic
+        Logic().run()
+    else:
+        from WebApp import app
+        app.run(host="127.0.0.1", port=5000)
