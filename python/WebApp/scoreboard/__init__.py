320da19fc49dbde06992375bb8e091a60706b1cf
Animcogn <corbanvilla@gmail.com>
2018-03-07 19:47:54 -0700

    creates database if none exists

--- python/WebApp/scoreboard/__init__.py
+++ python/WebApp/scoreboard/__init__.py
@@ -1,8 +1,10 @@
 from flask import current_app as app, render_template, request, redirect, jsonify, url_for, Blueprint, \
     abort, render_template_string, send_file
+from WebApp.scoreboard.database import init, insert
+
+init() #create database if none exists
 
 scoreboard = Blueprint('scoreboard', __name__)
-
 
 @scoreboard.route('/scoreboard', methods=['GET'])
 def admin_view():
