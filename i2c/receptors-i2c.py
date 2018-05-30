04784265478710834816d45d704b69c1ec16dc6b
Tyler Johnson <jtylers@byu.edu>
2018-05-29 20:13:42 -0600

    Got rid of submodule

---
+++ i2c/receptors-i2c.py
@@ -0,0 +1,62 @@
+import struct
+from array import array
+from time import sleep
+
+import smbus
+from lib.i2c.i2c_module import I2CModule
+
+
+class ReceptorControl(I2CModule):
+    RECEPTOR_COUNT = 6
+    REG = {
+        'Conversion': 0x00,
+        'Alert': 0x01,
+        'Config': 0x02,
+        'CycleTime': 0x03,
+        'Data1Low': 0x04,
+        'Data1High': 0x05,
+        'Hyst1': 0x06,
+        'Data2Low': 0x07,
+        'Data2High': 0x08,
+        'Hyst2': 0x09,
+        'Data3Low': 0x0A,
+        'Data3High': 0x0B,
+        'Hyst3': 0x0C,
+        'Data4Low': 0x0D,
+        'Data4High': 0x0E,
+        'Hyst4': 0x0F
+    }
+    CHANNEL = [0x80, 0x90, 0xA0, 0xB0, 0xC0, 0xD0, 0xE0, 0xF0]
+    CONFIG = [0x03, 0xF8]
+    READ_ALL = 0x70
+    # UNPACK_ALL = ''.join(['>'].extend(['H']*RECEPTOR_COUNT))
+    UNPACK_ALL = '>HHHHHH'
+
+    def __init__(self, bus, addr=0x21):
+        super().__init__(bus, addr)
+        self.receptors = [0] * self.RECEPTOR_COUNT
+        self.write_reg_bytes(self.REG['Config'], self.CONFIG)
+
+    def read_all(self):
+        _, data = self.read_reg_bytes(self.READ_ALL, 2 * self.RECEPTOR_COUNT)
+        self.receptors = list(struct.unpack(self.UNPACK_ALL, array('B', data).tostring()))
+        return self.receptors
+
+    def read(self, n):
+        sleep(.5)
+        if n >= self.RECEPTOR_COUNT:
+            return None
+        _, data = self.read_reg_bytes(self.CHANNEL[n], 2)
+        data2 = struct.unpack('>H', array('B', data).tostring())
+        self.receptors[n] = data2[0]
+        return self.receptors[n]
+
+
+if __name__ == '__main__':
+    bus = smbus.SMBus(1)
+    read = ReceptorControl(bus)
+    while True:
+        # print('{}, {}, {}, {}, {}, {}'.format(*[read.read(n) for n in range(6)]))
+        # print(read.read_all())
+        print(read.read(1))
+        sleep(1)
---
+++ i2c/sevenseg-i2c.py
@@ -0,0 +1,93 @@
+from time import sleep
+
+import smbus
+
+
+class SevenSegDisplay:
+    ADDR = 0x70
+    BLINK_CMD = 0x80
+    BLINK_DISPLAY_ON = 0x01
+    CMD_BRIGHTNESS = 0xE0
+    COLON = 0x02
+    CHARMAP = {
+        '0': 0x3F,
+        '1': 0x06,
+        '2': 0x5B,
+        '3': 0x4F,
+        '4': 0x66,
+        '5': 0x6D,
+        '6': 0x7D,
+        '7': 0x07,
+        '8': 0x7F,
+        '9': 0x6F,
+        'a': 0x77,
+        'b': 0x7C,
+        'c': 0x39,
+        'd': 0x5E,
+        'e': 0x79,
+        'f': 0x71}
+
+    def __init__(self, bus):
+        self.bus = bus
+        self.chars = bytearray(4)
+        self.dots = [False] * 4
+        self.colon = False
+        self.buf = [0] * 5
+
+    def set_digits(self, digits):
+        if len(digits) != 4:
+            return
+        for i, c in enumerate(digits):
+            self.chars[i] = self.CHARMAP[c]
+
+    def update(self):
+        self.buf = []
+        for i in range(4):
+            self.buf.append((self.dots[i] << 7) | self.chars[i])
+            self.buf.append(0)
+            if i == 1:
+                self.buf.append(self.COLON if self.colon else 0)
+                self.buf.append(0)
+        self.buf.extend([0] * 6)
+        # print(self.buf)
+        self.bus.write_i2c_block_data(self.ADDR, 0, self.buf)
+
+    def begin(self):
+        self.write_byte(0x21)  # start oscillator
+        self.blink_rate(0)
+        self.set_brightness(15)
+
+    def write_byte(self, byte):
+        self.bus.write_byte(self.ADDR, byte)
+
+    def set_brightness(self, level):
+        level = 0 if level > 15 else level
+        self.write_byte(self.CMD_BRIGHTNESS | level)
+
+    def blink_rate(self, rate=0):
+        rate = rate if rate <= 3 else 0
+        self.write_byte(self.BLINK_CMD | self.BLINK_DISPLAY_ON | (rate << 1))
+
+
+def main(argv):
+    bus = smbus.SMBus(1)
+    sevenseg = SevenSegDisplay(bus)
+    sevenseg.begin()
+    # sevenseg.blink_rate(1)
+    # sevenseg.dots = [True]*4
+    # sevenseg.colon = True
+    for n in range(1000):
+        s = str(n).rjust(4, '0')
+        # s = '2980'
+        sevenseg.set_digits(s)
+        sevenseg.update()
+        sleep(.01)
+
+    sevenseg.set_digits('beef')
+    sevenseg.update()
+
+
+if __name__ == '__main__':
+    import sys
+
+    main(sys.argv)
