1441a7c2950e6e54f75edb2fe914242c417c4269
Tyler Johnson <jtylers@byu.edu>
2018-06-08 12:23:40 -0600

    Test suite is fully mmmmm

--- i2c/test_suite.py
+++ i2c/test_suite.py
@@ -1,5 +1,6 @@
 import logging
 from argparse import ArgumentParser
+from threading import Thread
 from time import sleep
 from typing import List
 
@@ -16,15 +17,31 @@ logging.basicConfig(level=logging.INFO)
 
 
 class TestSuite:
+    DIGITS = 4
+    timer_running = False
+    _keypress = ["0"] * DIGITS
+    dead = False
     minutes = 0
     seconds = 0
-    _keypress = ""
 
     i2c_seven = None
     i2c_arduino = None
     i2c_lasers = None
     i2c_switches = None
     i2c_lock = None
+
+    def timer(self):
+        while True:
+            if self.timer_running:
+                if self.seconds:
+                    self.seconds -= 1
+                elif self.minutes:
+                    self.minutes -= 1
+                    self.seconds = 59
+                else:
+                    self.dead = True
+                    self.timer_running = False
+            sleep(0.9)
 
     def __init__(self, bus: SMBus):
         try:
@@ -52,7 +69,6 @@ class TestSuite:
 
         try:
             self.i2c_lock = BoxLock(bus)
-            self.i2c_lock.close()
             logger.info("Lock ready")
         except OSError:
             logger.warning("Solenoid setup failed")
@@ -72,22 +88,35 @@ class TestSuite:
         return self.i2c_switches.read_switches()[4:]
 
     @property
-    def keypress(self) -> str:
+    def keypress(self) -> List[str]:
         rcv = self.i2c_arduino.keypad
         for r in rcv:
             if r == '*':
                 if self.i2c_lock and self.i2c_lock.powered:
                     self.i2c_lock.open()
             elif r == "#":
-                """TODO Reset the timer"""
+                self.timer_running = True
             else:
-                self._keypress = r
+                self.timer_running = False
+                self._keypress.append(r)
+                self._keypress = self._keypress[-self.DIGITS:]
         return self._keypress
 
+    def run_lasers(self):
+        while True:
+            for x in range(0x40):
+                self.i2c_lasers.state = x
+                sleep(0.3)
+
     def run(self):
+        Thread(target=self.timer).start()
+        Thread(target=self.run_lasers).start()
+
         while True:
             sleep(.1)
-            self.i2c_seven(int("0x{}{}{:02}".format(self.keypress, self.minutes, self.seconds), 16), self.dots)
+            self.i2c_seven(int("0x{}".format(
+                "".join(self.keypress) if not self.timer_running else "{}{:02}".format(self.minutes, self.seconds)), 16)
+                , self.dots)
             rgb = self.rgb
             if not rgb[0] and not rgb[1]:
                 self.i2c_arduino.color = COLOR.BLANK
