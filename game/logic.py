f40d4c3c6ae9577bb5dd43936cba122685fe1711
Graigry Henrie <gwhenrie@gmail.com>
2018-09-11 21:58:32 -0600

    Adds new ways to die!
    The switches now correspond with how the RGB LED lights up to indicate the proper keypad code.
    Typing in the wrong code kills the player.
    
    Blocking a laser will kill the player.
    
    TODO: The updateLed function makes some assumptions as to how the switches return. A verification needs to take place as this may not work as Graigry thought.

--- game/logic.py
+++ game/logic.py
@@ -17,6 +17,7 @@ from Project_Theseus_API.i2c.lid_kit import ArduinoI2C
 from Project_Theseus_API.i2c.receptors_i2c import ReceptorControl
 from Project_Theseus_API.i2c.sevenseg import SevenSeg
 from Project_Theseus_API.i2c.lock_i2c import BoxLock
+from Project_Theseus_API.i2c.switches_i2c import SwitchesI2C
 if path.exists("/dev/i2c-1"):
     from smbus2 import SMBus
 else:
@@ -53,6 +54,7 @@ class Logic:
         self.arduino = ArduinoI2C(self._bus, I2C.ARDUINO)
         self.photo_resistors = ReceptorControl(self._bus, I2C.PHOTO_RESISTORS)
         self.lock = BoxLock(self._bus, I2C.SOLENOID)
+        self.switches = SwitchesI2C(self._bus, I2C.SWITCHES)
 
     @property
     def patternIndex(self) -> int:
@@ -162,6 +164,16 @@ class Logic:
         self.shared["logic"] = value.value
 
     @property
+    def entered_code(self) -> hex:
+        return self.shared.get("enteredcode", 0xFFF
+
+    @entered_code.setter
+    def entered_code(self, value: hex):
+        assert 0x0 <= value <= 0xfff
+        log.debug("Setting new entered code: 0x{}".format(value))
+        self.shared["enteredcode"] = value
+
+    @property
     def keypad_code(self) -> hex:
         return self.shared.get("code", 0x000)
 
@@ -218,27 +230,17 @@ class Logic:
             except KeyboardInterrupt:
                 return
 
-    def poll_sensors(self):
-        """
-        Poll all of the sensors and raise a flag if one of them has tripped.
-        If the right wire was clipped at the end of the puzzle, raise the win flag
-        """
-        """
-        for i in I2C:
-            word = self._bus.read_byte_data(self.bus_num, i)
-            if word is not None:
-                # log.info("{}: {}".format(i.name, hex(word)))
-                if i is I2C.RESET:
-                    ComQueue().getComQueue().put([INTERRUPT.TOGGLE_TIMER])
-                elif i is I2C.LASERS:
-                    if self.laserValue != word:
-                        ComQueue().getComQueue().put([INTERRUPT.KILL_PLAYER])
-        """
-
-        # self._bus.write_byte_data(I2C.LASERS.value, 0, 9) # for i2c in I2C:
-        #     log.debug("Reading from Project_Theseus_API.i2c on {}".format(i2c.name))
-        #     foo = self._bus.read_word_data(i2c.value, 0)
-        #     self._send(I2C.SEVEN_SEG, "Hello!")
+    # Commented out because we are doing it in other functions
+    # def poll_sensors(self):
+    #     """
+    #     Poll all of the sensors and raise a flag if one of them has tripped.
+    #     If the right wire was clipped at the end of the puzzle, raise the win flag
+    #     """
+    #
+    #     # self._bus.write_byte_data(I2C.LASERS.value, 0, 9) # for i2c in I2C:
+    #     #     log.debug("Reading from Project_Theseus_API.i2c on {}".format(i2c.name))
+    #     #     foo = self._bus.read_word_data(i2c.value, 0)
+    #     #     self._send(I2C.SEVEN_SEG, "Hello!")
 
     def getNextLaserPatternList(self):
         if self.laserState is LaserPattern.ONE_CYCLES:
@@ -301,6 +303,95 @@ class Logic:
         # Set laser pattern
         self.lasers.state = self.laserValue
 
+    def checkCode(self):
+        """
+        Need to verify that the code they entered was correct.
+
+        Verifies against the current keycode
+        """
+        # Get the last entered key
+        key = self.arduino.keypad
+        # The key should be a single hex character between 0 and 9
+        # The entered_code variable stores the 3 numbers they have entered
+        # If they enter a fourth number they die
+        # Start Game initializes it to 0xFFF so we check that it isn't F
+        # to see if a number has been previously entered
+        str_keypad_code = "{:2X}".format(self.keypad_code)
+        str_entered_code = "{:2X}".format(self.entered_code)
+
+        # Iterate through characters and verify
+        for i in range(0, 3):
+            # Add latest key to entered code
+            if str_entered_code[i] == "F":
+                if key != None:
+                    # We need to fill this space
+                    str_entered_code[i] = key
+                    # Save in the shared dict
+                    self.entered_code = int(str_entered_code)
+
+                    # Now we need to check if it matches
+                    if str_entered_code[i] != str_keypad_code[i]:
+                        return False
+                    else:
+                        # The key matched so far, continue
+                        pass
+                else:
+                    # Key was none, don't update entered and don't check
+                    pass
+
+            else:
+                # entered code is not F, so we need to see if it matches
+                # admitedly this shouldn't be called because of how we check
+                if str_entered_code[i] != str_keypad_code[i]:
+                    return False
+
+        # We are assuming it has matched so far, but if they have entered another key,
+        # it no longer does, and they should fail
+        if key != None:
+            return False
+        else:
+            # They didn't type a key, so they lived and won
+            # They didn't exceed the code size
+            # TODO: Add additional code for more logic later, this will
+            # make them win right now
+            self.state = STATE.WIN
+            self.end_game(success=True)
+        # If you reached here, the codes matched so far, so return True
+        return True
+
+    def updateLed(self):
+        """
+        There are 6 switches on the box. The four black correspond to a number
+        in hex, and the 2 white help you know which number in the keypad_code
+        you are looking at. The light will be RED if the white switches are 0
+        or the current code is wrong, and GREEN if the corresponding number from
+        keypad_code is correct.
+        """
+        # TODO: Verify that my understanding of how the switches are organized
+        # as far as the number they return is correct
+
+        # Determined by the white switches. 0 corresponds to none of the numbers
+        # and 1, 2, and 3 respectively correspond to the 1, 2, and 3 numbers in
+        # keypad_code
+        keypad_code_index = self.switches.read_switches()[-2:]
+        # What number does the user want to test?
+        test_number = self.switches.read_switches()[:-2]
+        # Get the keypad code as a string
+        str_keypad_code = "{:2X}".format(self.keypad_code)
+
+        # set the led based on if they are correct
+        if keypad_code_index > 0:
+            if str_keypad_code[keypad_code_index - 1] == str(test_number):
+                # It matches, make the light green
+                self.rgb_color = RGBColor.GREEN
+            else:
+                # It doesn't match, make the light red
+                self.rgb_color = RGBColor.RED
+        else:
+            # They haven't selected one, make it blue
+            self.rgb_color = RGBColor.BLUE
+
+
     def _loop(self):
         command_id = None
         if not self.comQueue.empty():
@@ -312,9 +403,14 @@ class Logic:
             self.sevenseg(0x0)
             self.laserState = LaserPattern.LASER_OFF
         elif self.state is STATE.RUNNING:
-            minutes, seconds, total_seconds = self.timer_values()
+            # Update the seven segment display to show the correct time
+            minutes, seconds, total_seconds = self.timer_values(
             self.sevenseg(int("0x{:02}{:02}".format(minutes, seconds), 16))
+            # Update what the current laser pattern should be
             self.updateLaserPattern()
+            # Update LED by checking switches
+            self.updateLed()
+
         elif self.state is STATE.EXPLODE:
             self.sevenseg(0xdead)
             # TODO randomize laser pattern so that they flash
@@ -345,9 +441,16 @@ class Logic:
 
             # Kill the player if time has run out
             elif total_seconds <= 0:
+                self.state = STATE.EXPLODE
                 self.end_game(success=False)
 
             elif self.laserValue != self.photo_resistors.read_int():
+                self.state = STATE.EXPLODE
+                self.end_game(success=False)
+
+            elif not self.checkCode():
+                # If they reached here the code didn't match
+                self.state = STATE.EXPLODE
                 self.end_game(success=False)
 
         elif self.state is STATE.EXPLODE:
@@ -373,8 +476,15 @@ class Logic:
 
     @staticmethod
     def random_laser_pattern() -> int:
-        # TODO make sure the laser pattern conforms to certain rules
         return random.randint(0, 0x40)
+
+    def generateKeyCode() -> int:
+        """
+        Generate a 3 number code where all numbers are less than 9 and >= 0
+        """
+        # We can't go over 9 because our keypad doesn't go above 9
+        return (random.randint(0, 0x9) << 8) | (random.randint(0, 0x9) << 4) | (random.randint(0, 0x9))
+
 
     def start_game(self):
         """
@@ -382,8 +492,9 @@ class Logic:
         """
         self.solenoid = SOLENOID_STATE.LOCKED
         self.start_time = datetime.now()
-        self.keypad_code = random.randint(0, 0xfff)
-        self.rgb_color = random.choice([RGBColor.RED, RGBColor.BLUE])
+        self.keypad_code = generateKeyCode()
+        self.entered_code = 0xFFF       # Set as this because it is impossible to get on our keypad
+        self.rgb_color = RGBColor.BLUE
         self.laserState = LaserPattern.ONE_CYCLES
         row = Row(
             name=self.team, lasers=self.laserValue, code=self.keypad_code, success=False, time=MAX_TIME,
